ea344d992666646943aa5d71a272476ce5ec2508
jvsobraz <jvsobraz@hotmail.com>
2024-07-15 20:05:17 -0300

    Implementação do Serviço "CpuService.ts" (lógica para monitorar a CPU)

--- src/app/cpu.service.ts
+++ src/app/cpu.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { cpuUsage } from "process";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class CpuService {
 
-  constructor() { }
+export class CpuService {
+  private cpuData: any = { cpuUsage: 0, processes: [] };
+
+  constructor() {
+    this.startMonitoring();
+  }
+
+  private startMonitoring() {
+    setInterval(() => {
+      this.cpuData.cpuUsage = Math.random() * 100; //Esta função simula o uso de CPU
+      this.cpuData.processes = this.getProcesses(); //Esta função simula os processos
+    }, 2000);
+  }
+
+  private getProcesses(): any[] {
+    return [
+      { name: "Processo A", cpuUsage: 20 },
+      { name: "Processo B", cpuUsage: 15 },
+      { name: "Processo C", cpuUsage: 30 }
+    ];
+  }
+
+  getCpuData() {
+    return this.cpuData;
+  }
 }
