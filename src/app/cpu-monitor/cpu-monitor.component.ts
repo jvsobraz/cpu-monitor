a32f2ee80d0aed84e36c49aea4c19c7f2ad9a68c
jvsobraz <jvsobraz@hotmail.com>
2024-07-15 20:09:09 -0300

    Exibição dos Dados no Componente "CpuMonitorComponent"

--- src/app/cpu-monitor/cpu-monitor.component.ts
+++ src/app/cpu-monitor/cpu-monitor.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CpuService } from '../cpu.service';
+import { cpuUsage } from 'process';
 
 @Component({
   selector: 'app-cpu-monitor',
-  standalone: true,
-  imports: [],
   templateUrl: './cpu-monitor.component.html',
   styleUrl: './cpu-monitor.component.css'
 })
-export class CpuMonitorComponent {
+export class CpuMonitorComponent implements OnInit {
+  cpuData: any = { cpuUsage: 0, processes: [] };
+
+  constructor(private cpuService: CpuService) {}
+
+  ngOnInit(): void {
+      this.cpuData = this.cpuService.getCpuData();
+      setInterval(() => {
+        this.cpuData = this.cpuService.getCpuData();
+      }, 2000);
+  }
 
 }
