e65293fdc894ebfcfd83d40b4fe7995c80402ed0
jvsobraz <jvsobraz@hotmail.com>
2024-07-15 19:57:56 -0300

    Criado o componente "cpu-monitor" e o serviço "cpu"

---
+++ src/app/cpu-monitor/cpu-monitor.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CpuMonitorComponent } from './cpu-monitor.component';
+
+describe('CpuMonitorComponent', () => {
+  let component: CpuMonitorComponent;
+  let fixture: ComponentFixture<CpuMonitorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CpuMonitorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CpuMonitorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/cpu-monitor/cpu-monitor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cpu-monitor',
+  standalone: true,
+  imports: [],
+  templateUrl: './cpu-monitor.component.html',
+  styleUrl: './cpu-monitor.component.css'
+})
+export class CpuMonitorComponent {
+
+}
